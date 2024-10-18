40f569f08de385a3d16ab506259a2de7db07b726
Omkar Pramod Hankare <86967995+Ompramod9921@users.noreply.github.com>
2024-10-18 10:08:28 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Embedding the iframe
+components.html(
+    """
+    <iframe
+        src="https://vismaya2939-texttoimage.hf.space"
+        frameborder="0"
+        width="850"
+        height="450"
+    ></iframe>
+    """,
+    height=500,  # Set the height of the iframe container
+)
